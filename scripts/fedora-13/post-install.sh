4a090a659ca10b90f88a194dc1ea224f9e008de3
Nathan O'Sullivan <nathan@mammoth.com.au>
2010-06-05 14:56:58 +1000

    fedora-13 post-install.sh: Copy of fedora 12 script

--- scripts/fedora-13/post-install.sh
+++ scripts/fedora-13/post-install.sh
@@ -52,24 +52,18 @@ if [ $ARCH = "amd64" ] ; then
     arch=x86_64
 fi
 
-echo "  Creating yum.conf"
+# A correct mirror URL does not contain /Packages on the end
+mirror=`dirname $mirror`
+
+echo "  Creating initial yum.conf"
 cat > ${prefix}/etc/yum.conf <<EOF
 [main]
-cachedir=/var/cache/yum
-debuglevel=1
 reposdir=/dev/null
 logfile=/var/log/yum.log
-retries=20
-obsoletes=1
-gpgcheck=0
-assumeyes=1
-
-# repos
 
 [core]
 name=core
 baseurl=$mirror
-
 EOF
 
 
@@ -90,9 +84,21 @@ cp /var/cache/rinse/fedora-13.$ARCH/* ${prefix}/var/cache/yum/core/packages/
 
 echo "  Bootstrapping yum"
 chroot ${prefix} /sbin/ldconfig
+chroot ${prefix} /sbin/MAKEDEV urandom
 chroot ${prefix} /usr/bin/yum -y install yum         2>/dev/null
 chroot ${prefix} /usr/bin/yum -y install vim-minimal 2>/dev/null
 chroot ${prefix} /usr/bin/yum -y install dhclient    2>/dev/null
+
+# Can use regular repositories now
+echo "  Creating final yum.conf"
+cat > ${prefix}/etc/yum.conf <<EOF
+[main]
+logfile=/var/log/yum.log
+gpgcheck=1
+
+# PUT YOUR REPOS HERE OR IN separate files named file.repo
+# in /etc/yum.repos.d
+EOF
 
 
 #
