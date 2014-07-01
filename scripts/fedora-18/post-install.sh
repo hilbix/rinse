e4b121ec768176dd191e55754cefa541a16e7fad
Thomas Lange <lange@informatik.uni-koeln.de>
2014-07-01 13:48:42 +0200

    fedora 18 uses f19 pist-install script

--- scripts/fedora-18/post-install.sh
+++ scripts/fedora-18/post-install.sh
@@ -4,7 +4,6 @@
 #
 
 
-
 prefix=$1
 
 if [ ! -d "${prefix}" ]; then
@@ -12,38 +11,36 @@ if [ ! -d "${prefix}" ]; then
   exit
 fi
 
-
-#
-#  2.  Copy the cached .RPM files into the yum directory, so that
-#     yum doesn't need to make them again.
-#
-echo "  Setting up YUM cache"
-if [ ! -d ${prefix}/var/cache/yum/core/packages/ ]; then
-    mkdir -p ${prefix}/var/cache/yum/core/packages/
-fi
-if [ ! -d ${prefix}/var/cache/yum/updates-released/packages/ ]; then
-    mkdir -p ${prefix}/var/cache/yum/updates-released/packages/
-fi
-
-for i in ${prefix}/*.rpm ; do
-    cp $i ${prefix}/var/cache/yum/core/packages/
-    cp $i ${prefix}/var/cache/yum/updates-released/packages/
-done
-
-
-
-#
-#  3.  Ensure that Yum has a working configuration file.
-#
 arch=i386
 if [ $ARCH = "amd64" ] ; then
     arch=x86_64
 fi
 
+#
+#  2.  Copy the cached .RPM files into the yum directory, so that
+#     yum doesn't need to download them again.
+#
+echo "  Setting up YUM cache"
+mkdir -p ${prefix}/var/cache/yum/core/packages/
+
+for i in ${prefix}/*.rpm ; do
+    cp -p $i ${prefix}/var/cache/yum/core/packages/
+done
+
+cp -pu $cache_dir/$dist.$ARCH/* ${prefix}/var/cache/yum/core/packages/
+
+
+#
+#  3.  Ensure that Yum has a working configuration file.
+#
+
+# use the mirror URL which was specified in rinse.conf
 # A correct mirror URL does not contain /Packages on the end
 mirror=`dirname $mirror`
 
-echo "  Creating initial yum.conf"
+# save original yum.conf
+mv ${prefix}/etc/yum.conf ${prefix}/etc/yum.conf.orig
+
 cat > ${prefix}/etc/yum.conf <<EOF
 [main]
 reposdir=/dev/null
@@ -59,31 +56,22 @@ EOF
 #  4.  Run "yum install yum".
 #
 
-echo "  Priming the yum cache"
-if [ ! -d "${prefix}/var/cache/yum/core/packages/" ]; then
-    mkdir -p ${prefix}/var/cache/yum/core/packages
-fi
-cp $cache_dir/$dist.$ARCH/* ${prefix}/var/cache/yum/core/packages/
-
 echo "  Bootstrapping yum"
 chroot ${prefix} /usr/bin/yum -y install yum vim-minimal dhclient
 
-# Can use regular repositories now
-echo "  Creating final yum.conf"
-cat > ${prefix}/etc/yum.conf <<EOF
-[main]
-logfile=/var/log/yum.log
-gpgcheck=1
+# restore original yum.conf
+mv ${prefix}/etc/yum.conf.orig ${prefix}/etc/yum.conf
 
-# PUT YOUR REPOS HERE OR IN separate files named file.repo
-# in /etc/yum.repos.d
-EOF
+
+# If you get this error, then replace https with http in /etc/yum.repos.d/*
+# Error: Cannot retrieve metalink for repository: fedora/19/x86_64. Please verify its path and try again
+#
+# sed -i -e 's/https/https' ${prefix}/etc/yum.repos.d/*
 
 
 #
 #  5.  Clean up
 #
-echo "  Cleaning up"
 chroot ${prefix} /usr/bin/yum clean all
 
 umount ${prefix}/proc
@@ -93,10 +81,7 @@ umount ${prefix}/sys
 #
 #  6.  Remove the .rpm files from the prefix root.
 #
-echo "  Final tidy..."
-for i in ${prefix}/*.rpm; do
-    rm -f $i
-done
+rm -f ${prefix}/*.rpm ${prefix}/var/cache/yum/core/packages/*.rpm
+
 find ${prefix} -name '*.rpmorig' -delete
 find ${prefix} -name '*.rpmnew' -delete
-
